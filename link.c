4f8dbc6d018920e01913dac6b9fa38e58e10d556
bilalkah <45990633+bilalkah@users.noreply.github.com>
2020-12-11 14:11:37 +0300

    Update link.c

--- link.c
+++ link.c
@@ -1,25 +1,3 @@
-/*  Creating matrix with sigly-linked-list
- *  x and y are indexes of the matrix and val is the value of the matrix at that index
- *  Example: 
- *  
- *  x=0
- *  y=0
- *  val=0
- *  output: 20
- * 
- *  x=1
- *  y=2
- *  val= 15
- *  20 -> 0 -> 0
- *  |    |    |
- *  V    V    V
- *  0 -> 0 -> 15
- *  
- *  C Language
- *  11/12/2020
- */
-
-
 #include<stdio.h>
 #include<stdlib.h>
 
